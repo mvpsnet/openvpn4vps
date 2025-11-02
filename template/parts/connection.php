e57e9973dd62f36aa81caa52ee69c11ba609d52f
MVPS.net <89784111+mvpsnet@users.noreply.github.com>
2025-11-02 22:13:34 +0200

    Update connection.php

--- template/parts/connection.php
+++ template/parts/connection.php
@@ -74,59 +74,9 @@ if (count(get_included_files()) == 1) {
                 </svg>
                 Download .ovpn file
             </a>
-
-            <button type="button" @click="hideQrCode = !hideQrCode"
-                    class="btn btn-xs sm:hidden my-1">
-                <svg xmlns="http://www.w3.org/2000/svg" class="-ml-1 mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/>
-                </svg>
-                <template x-if="hideQrCode"><span>Show QR Code</span></template>
-                <template x-if="!hideQrCode"><span>Hide QR Code</span></template>
-            </button>
         </div>
     </div>
 
-    <div class="my-1" x-data="{zoom: false}">
 
-        <canvas data-value="<?php
-$ca_cert = base64_decode($_config['ca_cert']);
-$ta_key = base64_decode($_config['ta_key']);
-$client_cert = base64_decode($profile['cert']);
-$client_key = base64_decode($profile['key']);
-$server_ip = $_SERVER['SERVER_ADDR'];
-$port = $_config['port'];
-$proto = $_config['proto'] ?? 'udp';
-
-echo "client\n";
-echo "dev tun\n";
-echo "proto $proto\n";
-echo "remote $server_ip $port\n";
-echo "resolv-retry infinite\n";
-echo "nobind\n";
-echo "persist-key\n";
-echo "persist-tun\n";
-echo "ca [inline]\n";
-echo "cert [inline]\n";
-echo "key [inline]\n";
-echo "tls-auth [inline] 1\n";
-echo "cipher AES-256-CBC\n";
-echo "auth SHA256\n";
-echo "comp-lzo\n";
-echo "verb 3\n";
-echo "\n";
-echo "<ca>\n$ca_cert</ca>\n";
-echo "<cert>\n$client_cert</cert>\n";
-echo "<key>\n$client_key</key>\n";
-echo "<tls-auth>\n$ta_key</tls-auth>\n";
-?>"
-             x-transition
-             @click="zoom = !zoom"
-             :style="{width: zoom ? '220px' : '150px'}"
-             class="sm:block transition-all qrcodes"
-             :class="{'hidden': hideQrCode, 'cursor-zoom-in': !zoom, 'cursor-zoom-out': zoom}"
-        ></canvas>
-    </div>
 
 </div>
